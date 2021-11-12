42ee72d8ec81438d2e54e01f118ecac3251239ff
Pedro Hdez <opencode00@gmail.com>
2021-11-12 20:07:38 +0000

    Nuevas estadisticas

--- PepaPig/euromillones_add_gsheet.py
+++ PepaPig/euromillones_add_gsheet.py
@@ -7,14 +7,14 @@ sheet = client.open("Euromillones").sheet1  # Open the spreadhseet
 index = sheet.row_count +1
 combinacion = euromillones_ayer()
 if (combinacion):
+    combinacion.append(f'=SUM(C{index}:G{index})')
     combinacion.append(f'=ABS(C{index}-D{index})')
     combinacion.append(f'=ABS(D{index}-E{index})')
     combinacion.append(f'=ABS(E{index}-F{index})')
     combinacion.append(f'=ABS(F{index}-G{index})')
-    combinacion.append(f'=if(countif($R$2:$R{index};R{index})>1;1;0)')
-    combinacion.append(f'=AVERAGE(C{index}:G{index})')
-    combinacion.append(f'=SUM(J{index}:M{index})')
-    combinacion.append(f'=AVERAGE(J{index}:M{index})')
+    combinacion.append(f'=SUM(K{index}:N{index})')
+    combinacion.append(f'=AVERAGE(K{index}:N{index})')
     combinacion.append(f'=CONCATENATE(C{index}:G{index})')
+    combinacion.append(f'=if(countif($R$2:$R{index};R{index})>1;1;0)')
     # print(combinacion)
     sheet.append_row(combinacion, 'USER_ENTERED', 'INSERT_ROWS', 'A:J')
