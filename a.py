e97ffebb14ecdd5429e5b386eb07bca6fd852d5c
Pedro Hdez <opencode00@gmail.com>
2022-02-24 14:33:35 +0000

    Protegiendo el API

--- a.py
+++ a.py
@@ -1,11 +1,25 @@
+import hashlib
+import os
+from dotenv import load_dotenv
+from datetime import datetime
 from flask import Flask, request, jsonify
 from Model import model
 app = Flask(__name__)
+load_dotenv()
 
+def protect(token):
+    key = os.getenv('KEY')+datetime.strftime(datetime.now(), "%H")
+    md5 = hashlib.md5()
+    md5.update(key.encode())
+    print(md5.hexdigest())
+    if token == md5.hexdigest():
+        return True
+    exit()
 
 @app.route('/listman/add', methods=["POST"])
 def listman_add(): #(tipo, name, value, name_data=None):
     #añade un valor nodo y dato
+    protect(request.args.get('key'))
     data = dict(request.form)
     if 'name_data' not in data.keys():
         data['name_data'] = data['name']
@@ -19,6 +33,7 @@ def listman_add(): #(tipo, name, value, name_data=None):
 @app.route('/listman/remove/<int:id>')
 def listman_remove(id):
     #elimina un nodo y los datos asociados
+    protect(request.args.get('key'))
     model.delete("nodes", id)
     model.delete("data", id, "id_node")
     return ''
@@ -26,6 +41,7 @@ def listman_remove(id):
 @app.route('/listman/get/<string:type>')
 def listman_get(type):
     #devuelve 
+    protect(request.args.get('key'))
     tipo = model.select("types", 'rowid', f"name like '{type}'")[0][0]
     node_id = model.select("nodes", "ROWID", f"type={tipo}")[0][0]
     data = model.get_full_data(f"A.type=1")
@@ -38,5 +54,6 @@ def add_header(r):
     r.headers['Access-Control-Allow-Headers'] = '*'
     r.headers['Access-Control-Allow-Origin'] = '*'
     return r
+
 
 app.run(port=5001)
